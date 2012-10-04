de952a62f65fca5492bb9d6b434188e5c272784f
Joost Kremers <joostkremers@fastmail.fm>
2012-10-04 15:03:00 +0200

    Create pre-commit hook

---
+++ precommit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git stash -q --keep-index
+
+pandoc --read=markdown \
+       --write=texinfo \
+       --output=/home/joost/src/writeroom-mode/writeroom-mode.texi \
+       --include-before-body=/home/joost/src/writeroom-mode/texi-before-body \
+       --standalone \
+       ./README.md
+
+RESULT=$?
+
+[ $RESULT == 0 ] && makeinfo ./writeroom-mode.texi && git add ./writeroom-mode.info
+
+git stash pop -q
+
+[ $RESULT -ne 0 ] && exit 1
+
+exit 0
