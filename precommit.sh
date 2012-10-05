253f456402273a83eb4d822914663707dbfc51de
Joost Kremers <joostkremers@fastmail.fm>
2012-10-05 12:03:21 +0200

    pre-commit hook: check if info file is older than README.md before regenerating it.

--- precommit.sh
+++ precommit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ README.md -ot writeroom-mode.info ] ; then
+    exit 0 # everything's fine
+fi
+
+# otherwise generate info file and git-add it.
+
 git stash -q --keep-index
 
 pandoc --read=markdown \
