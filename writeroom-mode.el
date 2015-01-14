f69398953cf5545917cdcf7239234f125dccf5bb
Joost Kremers <joostkremers@fastmail.fm>
2015-01-15 00:29:37 +0100

    Update version number to 2.7

--- writeroom-mode.el
+++ writeroom-mode.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Joost Kremers <joostkremers@fastmail.fm>
 ;; Created: 11 July 2012
 ;; Package-Requires: ((emacs "24.1") (visual-fill-column "1.1"))
-;; Version: 2
+;; Version: 2.7
 ;; Keywords: text
 
 ;; Redistribution and use in source and binary forms, with or without
