e7fba5c6dd6db28fface74d6a3de7245aff50df7
Joost Kremers <joostkremers@fastmail.fm>
2020-10-08 11:18:25 +0200

    Update Emacs dependency to 25.1.

--- writeroom-mode.el
+++ writeroom-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Joost Kremers <joostkremers@fastmail.fm>
 ;; Maintainer: Joost Kremers <joostkremers@fastmail.fm>
 ;; Created: 11 July 2012
-;; Package-Requires: ((emacs "24.1") (visual-fill-column "1.9"))
+;; Package-Requires: ((emacs "25.1") (visual-fill-column "1.9"))
 ;; Version: 3.9
 ;; Keywords: text
 ;; URL: https://github.com/joostkremers/writeroom-mode
