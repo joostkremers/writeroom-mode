5ca99c6d2653a4cb2b1e9d6001bb221c43e5c862
Joost Kremers <joostkremers@fastmail.fm>
2020-12-28 12:26:35 +0100

    Update version number in Commentary section.

--- writeroom-mode.el
+++ writeroom-mode.el
@@ -37,7 +37,7 @@
 
 ;; writeroom-mode is a minor mode for Emacs that implements a
 ;; distraction-free writing mode similar to the famous Writeroom editor for
-;; OS X. writeroom-mode is meant for GNU Emacs 24 and isn't tested on older
+;; OS X. writeroom-mode is meant for GNU Emacs 25 and isn't tested on older
 ;; versions.
 ;;
 ;; See the README or info manual for usage instructions.
